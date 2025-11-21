921125cad6f6ca5fd28b830c058f810d61ebe894
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2025-11-21 00:16:04 -0300

    configurei o ambiente e criei os testes para validar o metodo post e get

---
+++ cypress/e2e/Users/GET.cy.js
@@ -0,0 +1,62 @@
+import 'cypress-mochawesome-reporter/register';
+
+class SubjectApi {
+    requestHTTP = ({ method, url }) => {
+        return () => {
+            return cy.api({
+                method,
+                url,
+            });
+        }
+    }
+
+}
+
+
+
+
+describe("MetHod GET", () => {
+    it("Shold get all users", () => {
+        const api = new SubjectApi()
+        const request = api.requestHTTP({
+            method: "GET",
+            url: "/Users"
+        })
+
+        return request().then((res) => {
+            for (let i = 0; i < res.body.length; i++) {
+                cy.wrap(res.status).should("eq", 200)
+                cy.wrap(res.body[i]).should("include.keys", [
+                    "address",
+                    "id",
+                    "email",
+                    "username",
+                    "password",
+                    "name",
+                    "phone"
+                ]);
+            }
+        });
+    });
+
+    it("Shold get one user of", () => {
+        const api = new SubjectApi()
+        const request = api.requestHTTP({
+            method: "GET",
+            url: "/Users/1"
+        });
+
+        return request().then((res) => {
+            cy.wrap(res.status).should("eq", 200)
+            cy.wrap(res.body).should("include.keys", [
+                "address",
+                "id",
+                "email",
+                "username",
+                "password",
+                "name",
+                "phone"
+            ]);
+        });
+    });
+});
---
+++ cypress/e2e/Users/POST.cy.js
@@ -0,0 +1,46 @@
+import 'cypress-mochawesome-reporter/register';
+import { faker } from '@faker-js/faker';
+
+class SubjectApi {
+    requestHTTP = ({method, url, body}) => {
+        return () => {
+            return cy.api({
+                method,
+                url,
+                body
+            });
+        }
+    }
+}
+
+describe("Method POST", () => {
+    it("Should create a user", () => {
+        const api = new SubjectApi()
+        const createUser = {
+            username: faker.internet.username(),
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        }
+        const requestPOST = api.requestHTTP({
+            method: "POST",
+            url: "/Users",
+            body: createUser
+        
+        })
+        
+        return requestPOST().then((res) => {
+            cy.wrap(res.status).should("eq", 201)
+            cy.wrap(res.body).should("have.keys", "id")
+        
+            const id = res.body.id
+        
+            const requestGET = api.requestHTTP({
+                method: "GET",
+                url: `/Users/${id}`
+            })
+            return requestGET().then((res) => {
+                cy.wrap(res.status).should("eq", 200)
+            })
+        })
+    });
+});
