773d6b93416d0206f301dd394ba5a0e59fee448e
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2025-11-23 12:37:08 -0300

    Aqui validei a requisição post do carrinho

---
+++ cypress/e2e/Cart/POST.cy.js
@@ -0,0 +1,37 @@
+import 'cypress-mochawesome-reporter/register';
+import { faker } from '@faker-js/faker';
+
+class SubjectApi {
+    requestHTTP = ({ method, url, body }) => {
+        return () => {
+            return cy.api({
+                method,
+                url,
+                body
+            });
+        }
+    }
+}
+
+describe("Method POST", () => {
+    it("Should to add a product in cart", () => {
+        const api = new SubjectApi()
+        const createProduct = {
+            id: 10,
+            userId: 1,
+            products: [
+                {productId: 10, quantity: 5},
+                {productId: 13, quantity: 5}
+            ]
+        }
+        const requestPOST = api.requestHTTP({
+            method: "POST",
+            url: "/carts",
+            body: createProduct
+        })
+        return requestPOST().then((res) => {
+            cy.wrap(res.status).should("eq", 201)
+            cy.wrap(res.body).should("have.keys", ["id", "userId", "products"])
+        })
+    });
+});
