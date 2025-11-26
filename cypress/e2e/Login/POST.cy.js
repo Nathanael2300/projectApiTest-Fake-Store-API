069cda40061cb3a2cdc2f06bdaeb723fbae8778d
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2025-11-26 14:24:22 -0300

    Aqui validei a requisição Post(auth/login)

---
+++ cypress/e2e/Login/POST.cy.js
@@ -0,0 +1,33 @@
+import 'cypress-mochawesome-reporter/register';
+
+class SubjectApi {
+    requestHTTP = ({ method, url, body, failOnStatusCode }) => {
+        return () => {
+            return cy.api({
+                method,
+                url,
+                body,
+                failOnStatusCode
+            });
+        }
+    }
+}
+
+describe("MetHod POST", () => {
+    it("Should do login wich success", () => {
+        const api = new SubjectApi();
+        const credentials = { username: 'johnd', password: 'm38rmF$' };
+
+        const requestPOST = api.requestHTTP({
+            method: "POST",
+            url: "auth/login",
+            headers: { 'Content-Type': 'application/json' },
+            body: credentials,
+            failOnStatusCode: false
+        })
+        return requestPOST().then((res) => {
+            cy.wrap(res.status).should("eq", 200);
+            cy.wrap(res.body).should("have.a.property", "token")
+        })
+    });
+});
