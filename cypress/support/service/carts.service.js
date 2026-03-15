ae26db5587be43280189f440db2bc36ecb3c8dbd
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-03-15 01:00:49 -0300

    feat: adiciona função que faz requisição para /carts

---
+++ cypress/support/service/carts.service.js
@@ -0,0 +1,42 @@
+class CartsService {
+  getAllCarts() {
+    return cy.api({
+      method: "GET",
+      url: "/carts",
+    });
+  }
+
+  getCartById(id, options = {}) {
+    return cy.api({
+      method: "GET",
+      url: `carts/${id}`,
+      failOnStatusCode: options.failOnStatusCode ?? true,
+    });
+  }
+
+  createCart(data, options = {}) {
+    return cy.api({
+      method: "POST",
+      url: "/carts",
+      body: data,
+      failOnStatusCode: options.failOnStatusCode ?? true,
+    });
+  }
+
+  updataCart(id, data, options = {}) {
+    return cy.api({
+      method: "PUT",
+      url: `/carts/${id}`,
+      body: data,
+      failOnStatusCode: options.failOnStatusCode ?? true,
+    });
+  }
+
+  delete(id, options = {}) {
+    return cy.api({
+      method: "DELETE",
+      url: `/carts/${id}`,
+      failOnStatusCode: options.failOnStatusCode ?? true,
+    });
+  }
+}
