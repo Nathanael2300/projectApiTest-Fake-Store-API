2fef51de6d96b7151cd5a8d04c2995a70a344607
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-03-15 01:02:38 -0300

    feat: adiciona função que faz requisição para /products

---
+++ cypress/support/service/products.service.js
@@ -0,0 +1,34 @@
+class ProductsService {
+  getAllProducts() {
+    return cy.api({
+      method: "GET",
+      url: "/products",
+    });
+  }
+
+  getProductsById(id, options = {}) {
+    return cy.api({
+      method: "GET",
+      url: `/products/${id}`,
+      failOnStatusCode: options.failOnStatusCode ?? true,
+    });
+  }
+
+  createProducts(data, options = {}) {
+    return cy.api({
+      method: "POST",
+      url: "/products",
+      body: data,
+      failOnStatusCode: options.failOnStatusCode ?? true,
+    });
+  }
+
+  deleteProducts(id, options = {}) {
+    return cy.api({
+      method: "DELETE",
+      url: `products/${id}`,
+      failOnStatusCode: options.failOnStatusCode ?? true,
+    });
+  }
+}
+export default new ProductsService();
